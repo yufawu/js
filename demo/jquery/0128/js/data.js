87e6a0e9eac69492a9626b4aa2f6461614e0a1a4
yufawu <13763048459@163.com>
2020-09-02 21:49:17 +0800

    酷黑音乐播放器（jQuery）

---
+++ demo/jquery/0128/js/data.js
@@ -0,0 +1,4 @@
+//DATA("歌名","地址","ID");
+DATA("You Raise Me Up","http://www.htmldivcss.com/","1");
+DATA("The Glorious Death","http://www.htmldivcss.com/","2");
+DATA("My Love","http://www.htmldivcss.com/","3");
